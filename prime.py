e1fabda446553d5e2a12bb54e0df8a6bb280f2a6
Veikko Nyfors <veikkonyfors@gmail.com>
2021-03-19 07:52:11 +0000

    Amendments

--- prime.py
+++ prime.py
@@ -1,14 +1,23 @@
+# Prime, print prime numbers up to a limit
+
 psf=[1,2,3] # Primes So Far
 i=0 # Iterations
+nc=5 # Next Candidate
+limit=10000
 
 while True:
     i=i+1;
-    np=psf[len(psf)-1]+2; # Next Prime
+    nc=nc+2; # Next Prime
+
     # Is this one a multiple of a previous prime
-    for j in range(2,len(psf)):
-        print(np,psf[j],j,np%psf[j])
-        if np%psf[j]!=0 :
-            print(np) 
-            continue
+    for j in range(1,len(psf)):
+        #print(nc,psf[j],j,nc%psf[j],len(psf))
+        if nc%psf[j]==0 :break
+            
+    if nc%psf[j]!=0 :
+        psf.append(nc)
+    else: continue
     
-    if(i>=100): break;
+    if(i>=limit): break;
+        
+print(psf)
